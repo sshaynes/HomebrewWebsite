e22ddd62bef77c2322c765a909d679c73ba0bc0e
Zdravko Tyankov <ztyankov@gmail.com>
2014-09-15 17:25:41 -0400

    Add directives and templates for footer and header. Fix version interpolation filter.

--- js/directives.js
+++ js/directives.js
@@ -4,8 +4,29 @@
 
 
 angular.module('brewApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+directive('appVersion', ['version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  }
+}]).
+directive('ngHeader', function () {
+    return {
+        restrict: 'A', //This means that it will be used as an attribute and NOT as an element
+        replace: true,
+        scope: {user: '='}, // This is one of the cool things :). Will be explained in post.
+        templateUrl: "/static/partials/header.html",
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+            // Your behaviour goes here :)
+        }]
+    }
+}).
+directive('ngFooter', function () {
+    return {
+        restrict: 'A', //This will be used as an attribute and NOT as an element
+        replace: true,
+        templateUrl: "/static/partials/footer.html",
+        controller: ['$scope', '$filter', function ($scope, $filter) {
+            // Your behaviour goes here :)
+        }]
+    }
+});
--- js/filters.js
+++ js/filters.js
@@ -3,24 +3,22 @@
 /* Filters */
 
 angular.module('brewApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
-
+filter('interpolate', ['version', function(version) {
+  return function(text) {
+    return String(text).replace(/\%VERSION\%/mg, version);
+  }
+}]).
 // Create a new filter that shows the unfinished tasks
 // And shows the finished tasks when the flag is ON
-angular.module('brewApp.filters', []).
-  filter('checkedItems', function()
-  {
-    return function(items, showComplete) {
-      var resultArr = [];
-      angular.forEach(items, function(item) {
-        if(item.done == false || showComplete == true) {
-          resultArr.push(item);
-        }
-      });
-      return resultArr;
-    }
-  });
+filter('checkedItems', function()
+{
+  return function(items, showComplete) {
+    var resultArr = [];
+    angular.forEach(items, function(item) {
+      if(item.done == false || showComplete == true) {
+        resultArr.push(item);
+      }
+    });
+    return resultArr;
+  }
+});
