92a676e7ecdbe6e0753a90d06793c90b185bf3e5
Zdravko Tyankov <ztyankov@gmail.com>
2015-05-09 17:32:17 -0400

    Implement user registration with API calls to the server. Handle form validation both on the client and the server side and display errors as appropriate.

--- app/user/user.js
+++ app/user/user.js
@@ -5,28 +5,70 @@
         .module('app.user')
         .controller('User', User);
 
-    User.$inject = ['$timeout', 'config', 'logger', 'UserRepository'];
+    User.$inject = ['$q', '$location', 'config', 'logger', 'UserRepository', 'CLIENT_URLS'];
 
-    function User($timeout, config, logger, UserRepository) {
+    function User($q, $location, config, logger, UserRepository, CLIENT_URLS) {
         /*jshint validthis: true */
         var vm = this;
+        vm.userRegister = userRegister;
+        vm.editUserProfile = editUserProfile;
 
         var madeUpUserId = 123;
+        var userProfile = {};
 
-        getUserProfileData(madeUpUserId);
+        activate();
 
-        function getUserProfileData(userId)
-        {
-            var userProfile = UserRepository.getUserProfileData(userId);
+        // Setup and activate the module
+        function activate() {
+            UserRepository.getUserProfileData(madeUpUserId).then(function(data){
+                vm.userProfile = data;
+            });
 
-            vm.userProfile = userProfile;
+            logger.info('Activated User Module.');
         }
+
 
         function editUserProfile(formData)
         {
-          var errorsArr = [];
-          logger.info(formData);
+            var errorsArr = [];
+
+
+
+            logger.info(formData);
         }
+
+
+        function userRegister(formData)
+        {
+            var errorsArr = [];
+
+            errorsArr = UserRepository.validateUserRegistration(formData);
+
+            // No errors, register user
+            if(errorsArr.length == 0) {
+
+                logger.info("userRegister client side validation has passed!");
+
+                UserRepository.createNewUserAccount(formData).then(function (response){
+                    response = response.data;
+
+                    // Extract the value and create an array of status codes
+                    if(response.status == 200) {
+                        console.log("Success: " + response.status + " - " + response.message);
+                        $location.path(CLIENT_URLS.userProfile);
+                    }else{
+                        console.log("Fail: " + response.status + " - " + response.message);
+                        errorsArr.push(response.message);
+                    }
+                }, function (response){
+                    console.log("Fail: " + response.status + " - " + response.message);
+                    errorsArr.push(response.message);
+                });
+            }
+
+            vm.formErrors = errorsArr;
+        }
+
     };
 
 })();
