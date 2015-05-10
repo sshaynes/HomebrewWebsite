95035fa7c509642ade7c00a89175b4a6b465e274
Zdravko Tyankov <ztyankov@gmail.com>
2015-05-10 15:47:58 -0400

    Add a costants array for API_URLS and CLIENT_URLS to be used throughout the front-end.

--- app/core/constants.js
+++ app/core/constants.js
@@ -6,6 +6,7 @@
         // Binds the name of the Toastr module
         // It's used for notifications
         .constant('toastr', toastr)
+
         .constant('AUTH_EVENTS', {
             loginSuccess: 'auth-login-success',
             loginFailed: 'auth-login-failed',
@@ -14,9 +15,25 @@
             notAuthenticated: 'auth-not-authenticated',
             notAuthorized: 'auth-not-authorized'
         })
+
         .constant('CLIENT_URLS',{
-            userProfile: '/profile'
+            userProfile: '/profile',
+            userLogin: '/login',
+            userRegister: '/register',
         })
+
+        .constant('API_URLS',{
+            userCreate: 'user/create/',
+            userDelete: 'user/delete/',
+            userUpdatePassword: 'user/updatePassword/',
+            userUpdateProfile: 'user/updateProfile/',
+            authLogin: 'auth/login/',
+            authLogout: 'auth/logout/',
+            authPasswordReset: 'auth/passwordReset/',
+            authIsSessionActive: 'auth/isSessionActive/',
+
+        })
+
         .constant('USER_ROLES', {
             all: '*',
             admin: 'admin',
